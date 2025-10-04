8194504e8282a053bc60f35601c56ba28f0ca756
willtrigo <willtrigo@gmail.com>
2025-10-04 18:29:45 -0300

    Fix: rename StreamWriter  Changes to be committed: 	modified:   Makefile 	renamed:    src/infrastructure/io/StreamWriter.cpp -> src/infrastructure/io/StreamWriterConstructor.cpp 	new file:   src/infrastructure/io/StreamWriterUtilities.cpp

--- ex00/src/infrastructure/io/StreamWriterConstructor.cpp
+++ ex00/src/infrastructure/io/StreamWriterConstructor.cpp
@@ -1,19 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   StreamWriter.cpp                                   :+:      :+:    :+:   */
+/*   StreamWriterConstructor.cpp                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 18:37:08 by dande-je          #+#    #+#             */
-/*   Updated: 2025/10/03 18:10:17 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/10/04 18:28:28 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "infrastructure/io/StreamWriter.hpp"
 
-#include <iostream>
-#include <ostream>
 #include <stdexcept>
 
 StreamWriter::StreamWriter() {}
@@ -24,12 +22,4 @@ StreamWriter::~StreamWriter() {}
 
 StreamWriter& StreamWriter::operator=(const StreamWriter& /*unused*/) {
   throw std::runtime_error("StreamWriter assignment is not allowed");
-}
-
-void StreamWriter::print(std::ostream& ostr, const std::string& str,
-                         bool newLine) {
-  ostr << str;
-  if (newLine) {
-    ostr << std::endl;
-  }
 }
